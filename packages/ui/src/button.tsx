9cd7a9de2837681900f2409046f38e9f1d4ea992
minhanhnguyen6266 <minhanhnguyen6266@gmail.com>
2024-06-06 08:30:47 +0700

    config turbo.json

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,3 +1,3 @@
 export default function Button() {
-  return <button className="bg-green-400">Click here??????1231232</button>
+  return <button className="bg-green-400">Click herreeeeee1092u80918siajsp</button>
 }
