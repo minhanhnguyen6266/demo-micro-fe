4036d1efc2ea49a51f7fc44b3f3912afecac0689
minhanhnguyen6266 <minhanhnguyen6266@gmail.com>
2024-07-01 14:19:49 +0700

    update route

--- packages/constant/src/index.ts
+++ packages/constant/src/index.ts
@@ -1,2 +1,2 @@
-export const ROUTE_TO_WEB = "http://localhost:3002/"
-export const ROUTE_TO_DOCS = "http://localhost:3001/"
+export const ROUTE_TO_WEB = "http://localhost:3000/";
+export const ROUTE_TO_DOCS = "http://localhost:3001/";
